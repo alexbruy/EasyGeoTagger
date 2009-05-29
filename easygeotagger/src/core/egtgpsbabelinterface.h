ae8db754a0d81e5f81d4c4b57f718fe07a420a91
rgarcia <rgarcia@622b0c1a-4c31-43e3-84e9-cb026a211916>
2009-05-29 19:38:14 +0000

    -moved egtgpsbabelinterface to the right place
    git-svn-id: file:///home/pete/devel/svn/easygeotagger/trunk@309 622b0c1a-4c31-43e3-84e9-cb026a211916

--- easygeotagger/src/core/egtgpsbabelinterface.h
+++ easygeotagger/src/core/egtgpsbabelinterface.h
@@ -1,4 +1,5 @@
 extern "C"
 {
   MS_DLL_SPEC int convert(const char* theInputFile, const char* theInputType, const char* theOutputFile, const char* theOutputType, const char* theErrorFile, const char* theInfoFile);
+  MS_DLL_SPEC int convertWithOptions(const char* theInputFile, const char* theInputType, const char* theOutputFile, const char* theOutputType, const char* theErrorFile, const char* theInfoFile, const char* theOptions);
 }
